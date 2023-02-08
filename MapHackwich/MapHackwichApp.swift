e2a44c7539848f38a0aab5f84487aa694d8eb641
isa-bolanos <isa.bolanos55@gmail.com>
2023-02-08 09:42:56 -0600

    Initial Commit

---
+++ MapHackwich/MapHackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapHackwichApp.swift
+//  MapHackwich
+//
+//  Created by isa bolanos on 2/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MapHackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
